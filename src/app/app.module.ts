4306c183e3f0d94f25306474aeccc4a7f923465b
Lewis Njagi <lewismaybach@gmail.com>
2021-02-09 10:59:53 +0300

    add httpClient to app module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,32 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HttpClientModule } from '@angular/common/http';
+import { RepositoryComponent } from './repository/repository.component';
+import { LandingPageComponent } from './landing-page/landing-page.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { FormsModule } from '@angular/forms';
+import { FormComponent } from './form/form.component';
+import { DateCountPipe } from './date-count.pipe';
+import { UppercaseDirective } from './uppercase.directive';
+import { SearchUserComponent } from './search-user/search-user.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    RepositoryComponent,
+    LandingPageComponent,
+    NavbarComponent,
+    FormComponent,
+    DateCountPipe,
+    UppercaseDirective,
+    SearchUserComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
