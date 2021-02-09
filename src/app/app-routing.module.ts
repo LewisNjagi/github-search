5616fc6fa71213b789e5fd6dd478483ec3138b1f
Lewis Njagi <lewismaybach@gmail.com>
2021-02-09 10:52:56 +0300

    add routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LandingPageComponent } from './landing-page/landing-page.component';
+import { SearchUserComponent } from './search-user/search-user.component';
 
-const routes: Routes = [];
+
+const routes: Routes = [
+  {path: 'landingpage', component: LandingPageComponent},
+  {path: 'search-user', component: SearchUserComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
