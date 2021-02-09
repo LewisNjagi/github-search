96ca8f14d1d00fc70ce4ff6d18aaf3a04c6e7af5
Lewis Njagi <lewismaybach@gmail.com>
2021-02-09 10:47:47 +0300

    add sevice

---
+++ src/app/github-http/github-request.service.ts
@@ -0,0 +1,44 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { environment } from "../../environments/environment";
+import { Repository } from '../repository';
+import { User } from '../user';
+import { resolve } from 'dns';
+import { promise } from 'protractor';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class GithubRequestService {
+
+  user?: User;
+  repository: Repository;
+  userName:string;
+
+  constructor(private http:HttpClient) {
+    this.repository = new Repository("");
+    this.userName = "LewisNjagi"
+  }
+  
+  gitRequest() {
+    interface ApiResponse{
+      name:string;
+      repos_url: string;
+      created_at:string;
+    }
+    let promise = new Promise((resolve,reject)=>{
+      this.http.get<ApiResponse>(environment.apiUrl + this.userName + "/repos" + "?access_token=" +  environment.apiKey).toPromise().then(response=>{
+        this.repository = response
+
+        resolve(response)
+      },
+      error=>{
+        this.repository.name = "Sorry"
+
+        reject(error)
+      })
+    })
+    return promise
+  }
+
+}
